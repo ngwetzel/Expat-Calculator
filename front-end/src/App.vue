ecb44b74284742919624a4934d4c1f8b15f84ad6
Nathan Wetzel <ngwetzel@gmail.com>
2022-02-22 15:48:16 -0500

    added router and store

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,26 +1,101 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to the Portugal Expat Calculator"/>
+  <div>
+
+    <nav class="nav">
+      <router-link class="logo" v-bind:to="{ name: 'home' }" v-if="$store.state.token != ''"><img src="./assets/impromptu_logo.png" alt="Expat Calculator" ></router-link>
+      <!-- <router-link class="course-li-link" v-bind:to="{ name: 'course-list' }" v-if="$store.state.token != ''">View Course List</router-link> -->
+      
+      <span class="user">{{ $store.state.user.username }}</span>
+      <router-link class="logout-icon" v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''" ><img src="./assets/logout-icon.png" alt="Logout Icon" ></router-link>
+    </nav>
+    <br>
+
+    <!-- <router-view class="main-body" v-bind:isTeacher="this.$store.state.isTeacher"/> -->
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<style> 
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+/*****************************NAVBAR STYLING IS BELOW***************************/
+.nav {
+  display: flex;
+  position: relative;
+  width: 100%;
+  margin-left: -0.8%;
+  padding-left: 5px;
+  padding-right: 2%;
+  background-size: 105%;
+  justify-content: center;
+  align-items: center;
+  transform: translateY(-13%);
+  background-image: linear-gradient( rgb(44, 44, 44), rgb(29, 29, 29));
+  padding-bottom: 1px;
 }
-</script>
+.logo {
+  margin-right: 8px;
+  margin-top:6px;
+  margin-left: 8px;
+  padding-bottom: 0px;
+}
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.course-li-link {
+  margin-left: 20px;
 }
+
+.user {
+  margin-left: auto;
+  margin-right: 10px;
+}
+/*****************************NAVBAR STYLING IS ABOVE***************************/
+
+/******************GENERIC STYLING FOR ENTIRE WEBSITE IS BELOW******************/
+.main-body {
+  height: 98%;
+  display: block;
+  margin-left: auto;
+  margin-top: 10px;
+  padding-bottom: 25px;
+  margin-right: auto;
+  padding-left: 10px;
+  padding-right: 10px;
+  background-image: linear-gradient(to bottom right, rgb(36, 36, 36), rgb(68, 68, 68));
+  border-style: solid;
+  border-width: 1px;
+  border-radius: 5px;
+  width: 65%;
+}
+body {
+  background: cadetblue;
+  background-position: center;
+  background-size: 100%;
+  color: rgb(221, 221, 197);
+  font-family: sans-serif;
+}
+
+a {
+  color: rgb(221, 221, 197);
+}
+
+a:visited {
+color: rgb(172, 172, 152);
+}
+
+button {
+  border-style: solid;
+  border-width: 1px;
+  border-radius: 3px;
+  padding: 5px;
+  background-color: rgb(43, 43, 43);
+  color: rgb(221, 221, 197);
+}
+
+button:hover {
+  border-style: solid;
+  border-width: 1px;
+  border-radius: 3px;
+  padding: 6px;
+  background-color: rgb(43, 43, 43);
+  color: rgb(221, 221, 197);
+}
+/******************GENERIC STYLING FOR ENTIRE WEBSITE IS ABOVE******************/
 </style>
